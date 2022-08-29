9e03981fa0fb00d467f99ca1831f1a95668b66df
Rodrigo Freire <rohfreire@hotmail.com>
2022-08-29 00:07:39 -0300

    dados de pratica

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,11 @@
+let nome
+let email
+let profissao
+let idade
+let sonho
+
+nome = "Rodrigo Freire de Andrade"
+email = "rodrigofandradee@gmail.com"
+profissao = "futuro programador fullstack"
+idade = 28
+sonho = "Meu sonho eh morar em Portugal, estou a muito tempo planejando e tenho certeza que vai dar certo"
