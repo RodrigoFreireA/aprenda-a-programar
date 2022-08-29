1a466b79d06b2f6b5257738ab9afb906bc9cb8f3
Rodrigo Freire <rohfreire@hotmail.com>
2022-08-29 00:17:44 -0300

    preenchendo dados js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,4 +8,11 @@ nome = "Rodrigo Freire de Andrade"
 email = "rodrigofandradee@gmail.com"
 profissao = "futuro programador fullstack"
 idade = 28
+lugar = "portugal"
 sonho = "Meu sonho eh morar em Portugal, estou a muito tempo planejando e tenho certeza que vai dar certo"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(sonho)
+console.log('Olá,' + nome + '! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para' + lugar +'. Confirma pra gente alguns dados? A sua idade é ' + idade + ', você é ' + profissao + 'e o seu email é ' + email + '? Ficamos no aguardo. Muito obrigado e boa viagem! :D')
