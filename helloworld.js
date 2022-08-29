244375ab863350829dbab676ace81625ad0c1fdd
Rodrigo Freire <rohfreire@hotmail.com>
2022-08-28 23:57:24 -0300

    adicionando pasta da terafa dia 1

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,10 @@
 console.log("nepossivle, agora vai")
 console.log("Consegui, nao vou parar!!")
+//treinando declaracao de variaveis
+let minhaVariavel
+let outraVariavel = 1
+
+minhaVariavel = "Estou aprendendo JavaScript?(JS)"
+
+console.log(minhaVariavel)
+console.log(outraVariavel)
